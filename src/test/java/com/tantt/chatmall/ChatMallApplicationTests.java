50f603db9a4c413ebcc489ca4c977ffd1abe56b0
tantt2021 <tantt2021@outlook.com>
2024-08-17 23:00:21 +0800

    init

---
+++ src/test/java/com/tantt/chatmall/ChatMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tantt.chatmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
